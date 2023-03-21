54acc3d3424946a88a11c9baea677b03bb01b977
ViniciusMdeiros <103541422+ViniciusMdeiros@users.noreply.github.com>
2023-03-21 15:41:04 -0300

    Third Commit

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,68 @@
+import Modal from 'react-modal';
+
+import { Container, TransactionTypeContainer } from './styles';
+
+import closeImg from '../../assets/botãoFechar.svg';
+import incomeImg from'../../assets/entradas.svg';
+import outcomeImg from'../../assets/saídas.svg';
+
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+    return (
+        <Modal 
+      isOpen={isOpen} 
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+      >
+            <button type="button" 
+            onClick={onRequestClose} 
+            className="react-modal-close"
+            >
+
+                <img src={closeImg} alt="Fechar modal" />
+
+            </button>
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input
+                placeholder='Título'
+                />
+
+                <input
+                type="number"
+                placeholder='Valor'
+                />
+
+                <TransactionTypeContainer>
+                    <button
+                    type="button"
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+
+                    <button
+                    type="button"
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
+
+                <input
+                placeholder='Categoria'
+                />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
+        </Modal>
+    );
+}
